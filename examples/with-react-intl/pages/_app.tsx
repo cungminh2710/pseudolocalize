ce84e68db5cfa8afea920808d3ad956a0dd5416d
Minh <cungminh27@gmail.com>
2022-06-15 09:17:10 +1000

    add examples

---
+++ examples/with-react-intl/pages/_app.tsx
@@ -0,0 +1,24 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import messages from '../compiled-lang/en.json';
+import { generateENXD, pseudoIntl } from '../../../dist';
+import { IntlProvider, RawIntlProvider } from 'react-intl';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  if (process.env.NODE_ENV === 'development') {
+    const intl = pseudoIntl(generateENXD, messages, 'en');
+    return (
+      <RawIntlProvider value={intl}>
+        <Component {...pageProps} />
+      </RawIntlProvider>
+    );
+  } else {
+    return (
+      <IntlProvider messages={messages} locale="en">
+        <Component {...pageProps} />
+      </IntlProvider>
+    );
+  }
+}
+
+export default MyApp;
