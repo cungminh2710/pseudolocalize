b640b4a33db84ee44aeb19b0e3d7e9d44f26e9bd
cungminh2710 <cungminh2710@users.noreply.github.com>
2022-08-02 05:29:40 +0000

    Prettified Code!

--- examples/with-react-intl/pages/_app.tsx
+++ examples/with-react-intl/pages/_app.tsx
@@ -5,20 +5,20 @@ import { generateENXD, pseudoIntl } from '@cungminh2710/pseudolocalize';
 import { IntlProvider, RawIntlProvider } from 'react-intl';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  if (process.env.NODE_ENV === 'development') {
-    const intl = pseudoIntl(generateENXD, messages, 'en');
-    return (
-      <RawIntlProvider value={intl}>
-        <Component {...pageProps} />
-      </RawIntlProvider>
-    );
-  } else {
-    return (
-      <IntlProvider messages={messages} locale="en">
-        <Component {...pageProps} />
-      </IntlProvider>
-    );
-  }
+	if (process.env.NODE_ENV === 'development') {
+		const intl = pseudoIntl(generateENXD, messages, 'en');
+		return (
+			<RawIntlProvider value={intl}>
+				<Component {...pageProps} />
+			</RawIntlProvider>
+		);
+	} else {
+		return (
+			<IntlProvider messages={messages} locale='en'>
+				<Component {...pageProps} />
+			</IntlProvider>
+		);
+	}
 }
 
 export default MyApp;
